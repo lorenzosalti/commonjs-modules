635bb825fb46a9cb8a67fdfb6cbfced414a726cb
lorenzosalti <lorenzosalti@gmail.com>
2025-03-24 15:52:54 +0100

    feat(people): import of functions from names.js and hobbies.js

---
+++ people.js
@@ -0,0 +1,21 @@
+const firstName = 'Lorenzo';
+
+const lastName = 'Salti';
+
+const hobbyOne = 'climbing';
+
+const hobbyTwo = 'cycling';
+
+const hobbyThree = 'hiking';
+
+// imports from external files
+const fullName = require("./names.js");
+
+const hobbies = require('./hobbies.js');
+
+
+
+console.log(fullName(firstName, lastName));
+
+console.log(hobbies(hobbyOne, hobbyTwo, hobbyThree));
+
